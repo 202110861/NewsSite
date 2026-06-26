836ec0bcec7863d4d9d0c25c9863639da57ee651
202110861 <dbsalsk2514@naver.com>
2026-06-26 15:01:30 +0900

    update: 홈페이지 영상·연예·포토·이벤트 캐러셀 섹션 활성화

--- version-4/src/pages/HomePage.tsx
+++ version-4/src/pages/HomePage.tsx
@@ -19,12 +19,12 @@ export default function HomePage() {
       <HotIssueTicker />
       <HeroHeadlines articles={heroArticles} />
 
-      {/* <NewsCarousel title="영상뉴스" articles={videoArticles} /> */}
+      <NewsCarousel title="영상뉴스" articles={videoArticles} />
       <NewsCarousel title="정치" articles={politicsArticles} />
       <NewsCarousel title="문화/전시" articles={cultureArticles} />
-      {/* <NewsCarousel title="연예/스포츠" articles={entertainmentArticles} />
+      <NewsCarousel title="연예/스포츠" articles={entertainmentArticles} />
       <NewsCarousel title="포토뉴스" articles={photoArticles} />
-      <NewsCarousel title="이벤트/행사" articles={eventArticles} /> */}
+      <NewsCarousel title="이벤트/행사" articles={eventArticles} />
 
       <SectionNewsGrid data={allSectionArticles} />
     </>
