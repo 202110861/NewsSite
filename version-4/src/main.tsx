3c38e8a49d4adc67eba37b5fc32cce7e8bc79c0f
202110861 <dbsalsk2514@naver.com>
2026-06-30 13:31:09 +0900

    update: react-snap 프리렌더 HTML을 hydrateRoot로 연결

--- version-4/src/main.tsx
+++ version-4/src/main.tsx
@@ -1,13 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot, hydrateRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+const container = document.getElementById("root");
+if (!container) throw new Error("Root element #root not found");
+
+const app = (
+  <HelmetProvider>
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </HelmetProvider>
+);
+
+if (container.hasChildNodes()) {
+  hydrateRoot(container, app);
+} else {
+  createRoot(container).render(app);
+}
