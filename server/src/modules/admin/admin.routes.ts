376be7b720192a6658ba8161e693558f4430bbf9
202110861 <dbsalsk2514@naver.com>
2026-07-07 17:09:16 +0900

    feat: 관리자 기사 생성·수정·삭제 라우트 추가

--- server/src/modules/admin/admin.routes.ts
+++ server/src/modules/admin/admin.routes.ts
@@ -1,77 +1,96 @@
-import { Router } from 'express'
-import { adminOnly } from '../../middlewares/adminOnly.middleware.js'
-import { authMiddleware } from '../../middlewares/auth.middleware.js'
-import * as adminService from './admin.service.js'
-import { rejectSchema, updateArticleSchema, type ArticleStatus } from './admin.service.js'
+import { Router } from "express";
+import { adminOnly } from "../../middlewares/adminOnly.middleware.js";
+import { authMiddleware } from "../../middlewares/auth.middleware.js";
+import * as adminService from "./admin.service.js";
 
-export const adminArticlesRouter = Router()
-adminArticlesRouter.use(authMiddleware, adminOnly)
+export const adminArticlesRouter = Router();
+adminArticlesRouter.use(authMiddleware, adminOnly);
 
-adminArticlesRouter.get('/', async (req, res, next) => {
+adminArticlesRouter.get("/", async (req, res, next) => {
   try {
-    const status = req.query.status as ArticleStatus | undefined
-    const articles = await adminService.listAdminArticles(status)
-    res.json(articles)
+    const status = req.query.status as adminService.ArticleStatus | undefined;
+    const articles = await adminService.listAdminArticles(status);
+    res.json(articles);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-adminArticlesRouter.get('/:id', async (req, res, next) => {
+adminArticlesRouter.post("/", async (req, res, next) => {
   try {
-    const article = await adminService.getAdminArticle(req.params.id)
-    res.json(article)
+    const input = adminService.createArticleSchema.parse(req.body);
+    const article = await adminService.createAdminArticle(input);
+    res.status(201).json(article);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-adminArticlesRouter.patch('/:id', async (req, res, next) => {
+adminArticlesRouter.post("/bulk-delete", async (req, res, next) => {
   try {
-    const input = updateArticleSchema.parse(req.body)
-    const article = await adminService.updateAdminArticle(req.params.id, input)
-    res.json(article)
+    const { ids } = adminService.bulkDeleteSchema.parse(req.body);
+    await adminService.bulkDeleteArticles(ids);
+    res.status(204).send();
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-adminArticlesRouter.patch('/:id/approve', async (req, res, next) => {
+adminArticlesRouter.get("/:id", async (req, res, next) => {
   try {
-    const article = await adminService.approveArticle(req.params.id)
-    res.json(article)
+    const article = await adminService.getAdminArticle(req.params.id);
+    res.json(article);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-adminArticlesRouter.patch('/:id/reject', async (req, res, next) => {
+adminArticlesRouter.patch("/:id", async (req, res, next) => {
   try {
-    const { reason } = rejectSchema.parse(req.body)
-    const article = await adminService.rejectArticle(req.params.id, reason)
-    res.json(article)
+    const input = adminService.updateArticleSchema.parse(req.body);
+    const article = await adminService.updateAdminArticle(req.params.id, input);
+    res.json(article);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-adminArticlesRouter.delete('/:id', async (req, res, next) => {
+adminArticlesRouter.patch("/:id/approve", async (req, res, next) => {
   try {
-    await adminService.deleteArticle(req.params.id)
-    res.status(204).send()
+    const article = await adminService.approveArticle(req.params.id);
+    res.json(article);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
-export const adminStatsRouter = Router()
-adminStatsRouter.use(authMiddleware, adminOnly)
-
-adminStatsRouter.get('/dashboard', async (_req, res, next) => {
+adminArticlesRouter.patch("/:id/reject", async (req, res, next) => {
   try {
-    const stats = await adminService.getDashboardStats()
-    res.json(stats)
+    const { reason } = adminService.rejectSchema.parse(req.body);
+    const article = await adminService.rejectArticle(req.params.id, reason);
+    res.json(article);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+adminArticlesRouter.delete("/:id", async (req, res, next) => {
+  try {
+    await adminService.deleteArticle(req.params.id);
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
+});
+
+export const adminStatsRouter = Router();
+adminStatsRouter.use(authMiddleware, adminOnly);
+
+adminStatsRouter.get("/dashboard", async (_req, res, next) => {
+  try {
+    const stats = await adminService.getDashboardStats();
+    res.json(stats);
+  } catch (err) {
+    next(err);
+  }
+});
