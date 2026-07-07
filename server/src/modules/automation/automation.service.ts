c8275969e3e769705da43a433399a91104136c9c
202110861 <dbsalsk2514@naver.com>
2026-07-07 17:09:38 +0900

    refactor: 자동화 기사 생성 시 bodyBlocks 포함

--- server/src/modules/automation/automation.service.ts
+++ server/src/modules/automation/automation.service.ts
@@ -1,17 +1,22 @@
-import { prisma } from '../../db/client.js'
-import { buildArticleCreateData, type CreateArticleInput } from '../articles/article.mapper.js'
+import { prisma } from "../../db/client.js";
+import {
+  buildArticleCreateData,
+  type CreateArticleInput,
+} from "../articles/article.mapper.js";
 
 export async function createAutomationArticle(input: CreateArticleInput) {
   return prisma.article.create({
-    data: buildArticleCreateData(input, 'PENDING_REVIEW'),
-    include: { section: true },
-  })
+    data: buildArticleCreateData(input, "PENDING_REVIEW"),
+    include: { section: true, bodyBlocks: true },
+  });
 }
 
-export async function createAutomationArticlesBatch(inputs: CreateArticleInput[]) {
-  const results = []
+export async function createAutomationArticlesBatch(
+  inputs: CreateArticleInput[],
+) {
+  const results = [];
   for (const input of inputs) {
-    results.push(await createAutomationArticle(input))
+    results.push(await createAutomationArticle(input));
   }
-  return results
+  return results;
 }
