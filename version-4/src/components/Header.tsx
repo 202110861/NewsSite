fbf5942a437ebe084488d52fab234af066f2d663
202110861 <dbsalsk2514@naver.com>
2026-07-16 18:07:58 +0900

    fix: build error 해결

--- version-4/src/components/Header.tsx
+++ version-4/src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { MastheadLinks } from "./MastheadBar";
+// import { MastheadLinks } from "./MastheadBar";
 
 interface HeaderProps {
   mobileNavOpen: boolean;
