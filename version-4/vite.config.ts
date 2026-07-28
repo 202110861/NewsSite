250e6a1c4f064c61bfe270d49bf0f5c89296c2d2
미나 <mina@minaui-MacBookPro.local>
2026-07-28 13:38:14 +0900

    test

--- version-4/vite.config.ts
+++ version-4/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,14 +10,15 @@ export default defineConfig({
       usePolling: true,
     },
     proxy: {
-      '/api': {
-        target: 'http://localhost:4000',
+      "/api": {
+        target: "http://localhost:4000",
         changeOrigin: true,
       },
-      '/uploads': {
-        target: 'http://localhost:4000',
+      "/uploads": {
+        target: "http://localhost:4000",
         changeOrigin: true,
       },
     },
   },
-})
+});
+//test
